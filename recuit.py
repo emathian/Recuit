7e057135205815d937aa461c32afde5b59e2139e
Emi <emilie.mathian@insa-lyon.fr>
2018-10-27 10:14:58 +0200

    Import

---
+++ recuit.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import matplotlib as mpl
+from matplotlib import cm
+from math import sqrt
+from math import exp, expm1 , log
+from numpy import random
+import pandas as pd
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import matplotlib.colors
+import numpy as np
