fb761414460d47243b8daa858dbb71229797a01e
Emi <emilie.mathian@insa-lyon.fr>
2018-10-28 19:22:33 +0100

    recuit avec palier f

--- recuit.py
+++ recuit.py
@@ -48,7 +48,39 @@ def recuit_f1 (x0, t0, k, kp ,  tmax , A_rate_max ):	# t => time and T=> Tempera
 		A_rate = len(x)/t # nombre de mouvement réellement effectué par rapport au nombre d'iéttération total
 	
 	return  x,f,t	
+
+def recuit_f1_p (x0, t0, k, kp, tmax , A_rate_max, m ):	# t => time and T=> Temperature
+	x=[x0]
+	f=[f_1(x0)]
+	t=t0
+	T = 1/t0
+	A_rate = 1	# suppose qu'on commence par réduire la fonction de cout
 	
+	while t < tmax  and A_rate > A_rate_max : # rmq nb max iter implique une condition sur la fct de cout
+		## palier
+		xp = []
+		S = 0
+		for i in range(m):
+			xc= x[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1)
+			S+= f_1(xc) - f_1(x[-1])
+			xp.append(xc)
+
+		DE = 1/m * S
+		xx = x[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1  )	
+		ff = f_1(xx)
+		if ff < f[-1]:
+			x.append(xx)
+			f.append(ff)
+		else :
+			if random.random() < kp * exp( -DE / (1000*T)):
+				x.append(xx)
+				f.append(ff)
+				
+		t+=1 	# Pas de convergence	
+		T  = 1 / t 		
+		A_rate = len(x)/t # nombre de mouvement réellement effectué par rapport au nombre d'iéttération total
+	
+	return  x,f,t		
 
 
 def recuit_g (x0, y0, t0, k, kp ,  tmax , A_rate_max ):	# t => time and T=> Temperature
@@ -163,6 +195,7 @@ if Which_question==4:
 	Y = np.arange(-5,5,0.2)
 	X, Y = np.meshgrid(X, Y)
 	Z= g(X,Y)
+	print('min g :',Z)
 
 
 	S = recuit_g (0, 0, 1, 1, 0.001 ,  10000 , 0.0001 )
@@ -176,14 +209,15 @@ if Which_question==4:
 	
 
 	
-	S1 = recuit_g ( 0,0, 1, 1, 0.5 , 100, 0.0001 )
+	S1 = recuit_g ( 0,0, 1, 1, 0.5 , 10000, 0.0001 )
 	strS1 = ('x0 = 0 ; y0 = 0 ;t0 = 1 ; k = 1 ; kp = 0.5 ; tmax =  100' )
-	S2 = recuit_g ( 0,0, 1, 0.1 , 0.5 , 100, 0.0001 )
+	S2 = recuit_g ( 0,0, 1, 0.1 , 0.5 , 10000, 0.0001 )
 	strS2 = ('x0 = 0 ; y0 = 0 ; t0 = 1 ; k = 0.1 ; kp = 0.5 ; tmax =  100' )
-	S3 = recuit_g ( 0,0, 1, 10, 0.2 , 100, 0.0001 )
+	S3 = recuit_g ( 0,0, 1, 10, 0.2 , 10000, 0.0001 )
 	strS3 = ('x0 = 0 ; y0 = 0 ; t0 = 1 ; k = 10 ; kp = 0.2 ; tmax =  100' )
-	S4 = recuit_g ( 0,0, 1, 1 , 0.1 , 100, 0.0001 )
+	S4 = recuit_g ( 0,0, 1, 1 , 0.1 , 10000, 0.0001 )
 	strS4 = ('x0 = 0 ; y0 = 0 ; t0 = 1 ; k = 10 ; kp = 0.1 ; tmax =  100' )
+
 	L_sol=[]
 	L_title=[]
 	L_sol.append(S1)
@@ -198,10 +232,86 @@ if Which_question==4:
 		fig = plt.figure(i+1)
 		ax = fig.gca(projection='3d') #to perform a 3D plot
 		surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=0, antialiased=False) #plot definition and options
-		ax.plot(L_sol[i][0],L_sol[i][1],L_sol[i][2], c='red')
+		ax.plot(L_sol[i][0][-1],L_sol[i][1][-1],L_sol[i][2][-1], '-o',c='red')
 		ax.set_xlabel('x')
 		ax.set_ylabel('y')
 		ax.set_zlabel('g(x,y)')
 		plt.title(L_title[i])
 	plt.show()
 	
+
+if Which_question==5:
+
+	S = recuit_f1_p ( -5, 1, 10, 0.5 , 10000, 0.0001 , 5)
+	S1 = recuit_f1( -5, 1, 10, 0.5 , 10000, 0.0001 )
+	X = np.arange(-6,6,0.1)
+	fig = plt.figure(0)
+	plt.plot(S[0][-1], S[1][-1] , 'k-x')
+	plt.plot(S1[0][-1], S1[1][-1] , 'c-x')
+	plt.plot(X, f_1(X),  c='red')
+	plt.xlabel('x')
+	plt.ylabel('f(x)')	
+	
+	print(S[1][-1])
+	print(S1[1][-1])
+
+	k=[15,1,10,10]
+	kp=[0.5,0.5,0.8,0.1]
+	for i in range(len(k)):
+		S= recuit_f1 ( 0.5, 1,k[i], kp[i] , 10000, 0.0001 )
+		S1= recuit_f1_p ( 0.5, 1,k[i], kp[i] , 10000, 0.0001 ,5)
+		strS = 'x0 = 0.5 ; t0 = 1 ; k = (%f); kp = (%f) ; tmax =  10000'  % (k[i] ,kp[i] )
+		print('len S', len(S[0]), 'lenS1', len(S1[0]))
+		fig = plt.figure(i+1)
+		plt.plot(S[0][-1], S[1][-1] , 'k-x')
+		plt.plot(S1[0][-1], S1[1][-1] , 'c-x')
+		plt.plot(X, f_1(X),  c='red')	
+		plt.xlabel('x')
+		plt.title(strS)
+		plt.ylabel('f(x)')	
+	
+	plt.show()
+
+
+if Which_question==6:
+	pass
+	#T = np.arange(0,1,0.01)
+	#fig = plt.figure(2)
+	#plt.plot(T, 0.1*np.exp(-1/(1000*T)),  c='g') # Aug variance
+	#plt.plot(T, 10*np.exp(-1/(1000*T)),  c='b') # Dim variance
+	#plt.xlabel('x')
+	#plt.ylabel('f(x)')	
+	#plt.show()
+
+	#print(min(S[1]))
+	#print((S[2]))
+
+	# S1 = recuit_f1 ( 0.5, 1, 15, 0.5 , 10000, 0.0001 )
+	# strS1 = ('x0 = 0.5 ; t0 = 1 ; k = 15 ; kp = 0.5 ; tmax =  10000' )
+	# S2 = recuit_f1 ( 0.5, 1, 1 , 0.5 , 10000, 0.0001 )
+	# strS2 = ('x0 = 0.5 ; t0 = 1 ; k = 1 ; kp = 0.5 ; tmax =  10000' )
+	# S3 = recuit_f1 ( 0.5, 1, 10, 0.8 , 10000, 0.0001 )
+	# strS3 = ('x0 = 0.5 ; t0 = 1 ; k = 10 ; kp = 0.8 ; tmax =  10000' )
+	# S4 = recuit_f1 ( 0.5, 1, 1 , 0.1 , 10000, 0.0001 )
+	# strS4 = ('x0 = 0.5 ; t0 = 1 ; k = 10 ; kp = 0.1 ; tmax =  10000' )
+	# L_sol=[]
+	# L_title=[]
+	# L_sol.append(S1)
+	# L_sol.append(S2)
+	# L_sol.append(S3)
+	# L_sol.append(S4)
+	# L_title.append(strS1)
+	# L_title.append(strS2)
+	# L_title.append(strS3)
+	# L_title.append(strS4)
+	# for i in range(0,5) :
+	# 	fig = plt.figure(i+3)
+	# 	plt.plot(L_sol[i][0], L_sol[i][1] , 'k-x')
+	# 	plt.plot(X, f_1(X),  c='red')	
+	# 	plt.xlabel('x')
+	# 	plt.title(L_title[i])
+	# 	plt.ylabel('f(x)')	
+	# plt.show()
+
+
+
