be82fd7ee0c05e7492e02534fa3398d21bff93bf
Emi <emilie.mathian@insa-lyon.fr>
2018-10-28 19:40:44 +0100

    recuit palier g

--- recuit.py
+++ recuit.py
@@ -58,12 +58,11 @@ def recuit_f1_p (x0, t0, k, kp, tmax , A_rate_max, m ):	# t => time and T=> Temp
 	
 	while t < tmax  and A_rate > A_rate_max : # rmq nb max iter implique une condition sur la fct de cout
 		## palier
-		xp = []
 		S = 0
 		for i in range(m):
 			xc= x[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1)
 			S+= f_1(xc) - f_1(x[-1])
-			xp.append(xc)
+		
 
 		DE = 1/m * S
 		xx = x[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1  )	
@@ -110,7 +109,43 @@ def recuit_g (x0, y0, t0, k, kp ,  tmax , A_rate_max ):	# t => time and T=> Temp
 		T  = 1 / t 		
 		A_rate = len(x)/t # nombre de mouvement réellement effectué par rapport au nombre d'iéttération total
 	
-	return  x,y,f,t			
+	return  x,y,f,t		
+
+def recuit_g_p (x0, y0,t0, k, kp, tmax , A_rate_max, m ):	# t => time and T=> Temperature
+	x=[x0]
+	y=[y0]
+	f=[g(x0,y0)]
+	t=t0
+	T = 1/t0
+	A_rate = 1	# suppose qu'on commence par réduire la fonction de cout
+	
+	while t < tmax  and A_rate > A_rate_max : # rmq nb max iter implique une condition sur la fct de cout
+		## palier
+		S = 0
+		for i in range(m):
+			xc= x[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1)
+			yc= y[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1)
+			S+= g(xc, yc) - g(x[-1], y[-1])
+			
+		DE = 1/m * S
+		xx = x[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1  )	
+		yy = y[-1] + np.random.normal(0, sqrt(k* exp(-1/(1000*T))) ,1  )	
+		ff = g(xx,yy)
+		if ff < f[-1]:
+			x.append(xx)
+			y.append(yy)
+			f.append(ff)
+		else :
+			if random.random() < kp * exp( -DE / (1000*T)):
+				x.append(xx)
+				y.append(yy)
+				f.append(ff)
+				
+		t+=1 	# Pas de convergence	
+		T  = 1 / t 		
+		A_rate = len(x)/t # nombre de mouvement réellement effectué par rapport au nombre d'iéttération total
+	
+	return  x,y,f,t				
 
 ###############################################################################################
 #								MAIN														  #	
@@ -274,44 +309,26 @@ if Which_question==5:
 
 
 if Which_question==6:
-	pass
-	#T = np.arange(0,1,0.01)
-	#fig = plt.figure(2)
-	#plt.plot(T, 0.1*np.exp(-1/(1000*T)),  c='g') # Aug variance
-	#plt.plot(T, 10*np.exp(-1/(1000*T)),  c='b') # Dim variance
-	#plt.xlabel('x')
-	#plt.ylabel('f(x)')	
-	#plt.show()
-
-	#print(min(S[1]))
-	#print((S[2]))
-
-	# S1 = recuit_f1 ( 0.5, 1, 15, 0.5 , 10000, 0.0001 )
-	# strS1 = ('x0 = 0.5 ; t0 = 1 ; k = 15 ; kp = 0.5 ; tmax =  10000' )
-	# S2 = recuit_f1 ( 0.5, 1, 1 , 0.5 , 10000, 0.0001 )
-	# strS2 = ('x0 = 0.5 ; t0 = 1 ; k = 1 ; kp = 0.5 ; tmax =  10000' )
-	# S3 = recuit_f1 ( 0.5, 1, 10, 0.8 , 10000, 0.0001 )
-	# strS3 = ('x0 = 0.5 ; t0 = 1 ; k = 10 ; kp = 0.8 ; tmax =  10000' )
-	# S4 = recuit_f1 ( 0.5, 1, 1 , 0.1 , 10000, 0.0001 )
-	# strS4 = ('x0 = 0.5 ; t0 = 1 ; k = 10 ; kp = 0.1 ; tmax =  10000' )
-	# L_sol=[]
-	# L_title=[]
-	# L_sol.append(S1)
-	# L_sol.append(S2)
-	# L_sol.append(S3)
-	# L_sol.append(S4)
-	# L_title.append(strS1)
-	# L_title.append(strS2)
-	# L_title.append(strS3)
-	# L_title.append(strS4)
-	# for i in range(0,5) :
-	# 	fig = plt.figure(i+3)
-	# 	plt.plot(L_sol[i][0], L_sol[i][1] , 'k-x')
-	# 	plt.plot(X, f_1(X),  c='red')	
-	# 	plt.xlabel('x')
-	# 	plt.title(L_title[i])
-	# 	plt.ylabel('f(x)')	
-	# plt.show()
+	X = np.arange(-5,5,0.2)
+	Y = np.arange(-5,5,0.2)
+	X, Y = np.meshgrid(X, Y)
+	Z= g(X,Y)
 
 
+	S = recuit_g_p ( 0,0, 1, 10, 0.5 , 10000, 0.0001 , 5)
+	S1 = recuit_g( 0,0, 1, 10, 0.5 , 10000, 0.0001 )
 
+	fig = plt.figure(0)
+	ax = fig.gca(projection='3d') #to perform a 3D plot
+	surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=0, antialiased=False) #plot definition and options
+	ax.plot(S[0][-1],S[1][-1],S[2][-1], '-o', c='red')
+	ax.plot(S1[0][-1],S1[1][-1],S1[2][-1], '-o',c='cyan')
+	ax.set_xlabel('x')
+	ax.set_ylabel('y')
+	ax.set_zlabel('z')
+	
+	
+	print(S[1][-1])
+	print(S1[1][-1])
+
+	plt.show()
