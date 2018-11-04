7184de47e5f73c92cac9042195c4bf32d1ac9d26
Emi <emilie.mathian@insa-lyon.fr>
2018-11-04 10:20:26 +0100

    first test for display

--- commerce_du_voyageur.py
+++ commerce_du_voyageur.py
@@ -17,7 +17,6 @@ from scipy.stats import ttest_ind
 from scipy.stats import t
 import statistics as s
 
-
 np.random.seed()
 random.seed()
 
@@ -30,7 +29,9 @@ def distance (Trajet, carte):
 	while len(Trajet) > 1 :
 		#print('carte[Trajet[0]][0]', carte[Trajet[0]][0] ,  'carte[Trajet[1]][0] ', carte[Trajet[1]][0],  'carte[Trajet[0]][1]', 'carte[Trajet[1]][1]',carte[Trajet[1]][1])
 		D += sqrt((carte[Trajet[0]][0]-carte[Trajet[1]][0])**2+(carte[Trajet[0]][1]-carte[Trajet[1]][1])**2 )
-		Trajet.pop(0)
+		#Trajet.pop(0)
+		Trajet = np.delete(Trajet, 0, 0)
+
 	return D	
 def random_journey (journey):
 	
@@ -41,7 +42,7 @@ def random_journey (journey):
 	journey[j] = old_journay_i
 	return journey
 
-def recuit_traveller (country , journey0, k, kp ,tmax, A_rate_max, m ):
+def recuit_traveller (country , journey0, t0,k, kp ,tmax, A_rate_max, m ):
 	j = [journey0]
 	d = [distance(journey0, country)]
 	t=t0
@@ -65,16 +66,71 @@ def recuit_traveller (country , journey0, k, kp ,tmax, A_rate_max, m ):
 			if random.random() < kp * exp( -DE / (1000*T)):
 				j.append(jj)
 				d.append(dd)
-				
+		
+
+		t+=1 	# Pas de convergence	
+		T  = 1 / t 		
+		A_rate = len(x)/t # nombre de mouvement réellement effectué par rapport au nombre d'iéttération total
+		
+	return  j,d,t		
+
+
+
+def recuit_traveller_display (country , journey0, t0, k, kp ,tmax, A_rate_max, m ):
+	j = [journey0]
+	d = [distance(journey0, country)]
+	t=t0
+	T = 1/t0
+	A_rate = 1	# suppose qu'on commence par réduire la fonction de cout
+	while t < tmax  and A_rate > A_rate_max : # rmq nb max iter implique une condition sur la fct de cout
+		## palier
+		S = 0
+		for i in range(m):
+			jc= random_journey(j[-1])
+			S+= distance(jc) - distance(j[-1])
+		
+
+		DE = 1/m * S
+		jj = random_journey(j[-1])
+		dd = distance(jj)
+		if dd < d[-1]:
+			j.append(jj)
+			d.append(dd)
+		else :
+			if random.random() < kp * exp( -DE / (1000*T)):
+				j.append(jj)
+				d.append(dd)
+
+		display(j[-1], country)
 		t+=1 	# Pas de convergence	
 		T  = 1 / t 		
 		A_rate = len(x)/t # nombre de mouvement réellement effectué par rapport au nombre d'iéttération total
 	
 	return  j,d,t		
 
-#macarte = (cities(3,3))
-#print(macarte)
-T1= [0,2,3,8,1,10]
+def display(journey , country):
+	sort_country = np.zeros((np.shape(country)[0], np.shape(country)[1]))
+	for i,j in zip(journey, range(len(country))):
+		sort_country[j,] = country[i,]
+
+	plt.plot(sort_country[:,0],sort_country[:,1],'-o')
+	plt.show()		
+
+
+macarte = (cities(3,10))
+print(macarte)
+T1= [0,3,4,6,1,2,9,7,8,5]
 #print(distance(T1, macarte))
-print(random_journey(T1))
+display(T1, macarte)
+
+
+
+
+S = recuit_traveller_display (macarte, T1, 1,10, 0.5 ,10000, 1*10**5, 5 )
+print(S)
+#fig = plt.figure(1)
+#plt.plot(macarte[:,0],macarte[:,1],'-o')
+# plt.xlabel('x')
+# plt.ylabel('f(x)')	
+#plt.show()	
 
